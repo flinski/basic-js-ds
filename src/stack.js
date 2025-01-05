52644fa9ba2f9650b7ea09a38fe351bab0ed8d16
Vladislav Flinski <flinssski@gmail.com>
2025-01-05 11:21:29 +0300

    feat: add solution

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.storage = []
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	push(value) {
+		this.storage.push(value)
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	pop() {
+		return this.storage.pop()
+	}
+
+	peek() {
+		return this.storage[this.storage.length - 1]
+	}
 }
 
 module.exports = {
-  Stack
-};
+	Stack,
+}
